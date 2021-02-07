a883b0e7c85acb8544f33bd72772e7fb02e1b6fe
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2021-02-07 13:57:08 -0500

    Setting up Database Testing
    Databse tests made but are failing. Fix coming.

---
+++ YourNewFavoritePoem.UnitTests/Tests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Your_New_Favorite_Poem;
+using Your_New_Favorite_Poem.Database;
+
+namespace YourNewFavoritePoem.UnitTests.Tests
+{
+    class BaseTest
+    {
+        AuthorsDbContext? authorsDbContext;
+
+        protected AuthorsDbContext AuthorsDbContext => authorsDbContext ?? throw new NullReferenceException();
+
+        [SetUp]
+        public void Setup()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AuthorsDbContext>()
+    .UseInMemoryDatabase("InMemoryDb");
+
+            authorsDbContext = new AuthorsDbContext(optionsBuilder.Options);
+            DbInitializer.Initialize(authorsDbContext);
+        }
+    }
+}
