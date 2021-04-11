820c863f0a49003b100f034f4bc9e11263b7b535
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2021-04-11 15:12:06 -0400

    working to generate guids for keys

--- Your New Favorite Poem/Migrations/20210322181140_UseGuidForPrimaryKey.cs
+++ Your New Favorite Poem/Migrations/20210322181140_UseGuidForPrimaryKey.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using MySql.EntityFrameworkCore.Metadata;
 
 namespace Your_New_Favorite_Poem.Migrations
@@ -8,6 +9,32 @@ namespace Your_New_Favorite_Poem.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            //need to change authorId to a guid as well. Ensure that
+            //authorID matches the newly genereated Id2
+            //can we pull a Guid that pulls the previously used guid for new guid 
+            //instead of creating a whole new guid for authorId
+            //https://www.codeproject.com/Articles/1193009/Changing-primary-key-data-type-in-EF-Core
+
+
+            migrationBuilder.AddColumn<Guid>("Id2", "authors", "TEXT", nullable: false, defaultValueSql: "newid()");
+            migrationBuilder.AddColumn<Guid>("Id2", "poems", "TEXT", nullable: false, defaultValueSql: "newid()");
+
+            migrationBuilder.DropForeignKey("FK_poems_authors_AuthorId", "poems");
+
+            migrationBuilder.DropPrimaryKey("PK_authors", "authors");
+            migrationBuilder.DropPrimaryKey("PK_poems", "poems");
+
+            migrationBuilder.DropColumn("Id", "authors");
+            migrationBuilder.DropColumn("Id", "poems");
+
+            migrationBuilder.RenameColumn("Id2", "authors", "Id");
+            migrationBuilder.RenameColumn("Id2", "poems", "Id");
+
+            migrationBuilder.AddPrimaryKey("PK_authors", "authors", "Id");
+            migrationBuilder.AddPrimaryKey("PK_poems", "poems", "Id");
+
+            migrationBuilder.AddForeignKey("FK_poems_authors_AuthorId", "poems", "AuthorId", "authors", principalColumn: "Id");
+
             migrationBuilder.AlterColumn<DateTimeOffset>(
                 name: "UpdatedAt",
                 table: "poems",
@@ -44,22 +71,22 @@ namespace Your_New_Favorite_Poem.Migrations
                 oldType: "timestamp",
                 oldDefaultValue: new DateTimeOffset(new DateTime(2020, 12, 20, 18, 4, 11, 112, DateTimeKind.Unspecified).AddTicks(9695), new TimeSpan(0, 0, 0, 0, 0)));
 
-            migrationBuilder.AlterColumn<byte[]>(
-                name: "AuthorId",
-                table: "poems",
-                type: "varbinary(16)",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "int");
+            //migrationBuilder.AlterColumn<byte[]>(
+            //    name: "AuthorId",
+            //    table: "poems",
+            //    type: "varbinary(16)",
+            //    nullable: false,
+            //    oldClrType: typeof(int),
+            //    oldType: "int");
 
-            migrationBuilder.AlterColumn<byte[]>(
-                name: "Id",
-                table: "poems",
-                type: "varbinary(16)",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "int")
-                .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+            //migrationBuilder.AlterColumn<byte[]>(
+            //    name: "Id",
+            //    table: "poems",
+            //    type: "varbinary(16)",
+            //    nullable: false,
+            //    oldClrType: typeof(int),
+            //    oldType: "int")
+            //    .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
 
             migrationBuilder.AlterColumn<DateTimeOffset>(
                 name: "UpdatedAt",
@@ -97,14 +124,14 @@ namespace Your_New_Favorite_Poem.Migrations
                 oldType: "timestamp",
                 oldDefaultValue: new DateTimeOffset(new DateTime(2020, 12, 20, 18, 4, 11, 109, DateTimeKind.Unspecified).AddTicks(63), new TimeSpan(0, 0, 0, 0, 0)));
 
-            migrationBuilder.AlterColumn<byte[]>(
-                name: "Id",
-                table: "authors",
-                type: "varbinary(16)",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "int")
-                .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+            //migrationBuilder.AlterColumn<byte[]>(
+            //    name: "Id",
+            //    table: "authors",
+            //    type: "varbinary(16)",
+            //    nullable: false,
+            //    oldClrType: typeof(int),
+            //    oldType: "int")
+            //    .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -207,5 +234,34 @@ namespace Your_New_Favorite_Poem.Migrations
                 oldType: "varbinary(16)")
                 .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
         }
+
+        public void Convert(MigrationBuilder migrationBuilder, string tableName, string pKConstraintName, params string[] children)
+        {
+            
+
+            //foreach (var child in children)
+            //{
+            //    DropForeignKey($"dbo.{child}s", $"{tableName}_Id", $"dbo.{tableName}s");
+            //    DropIndex($"dbo.{child}s", new[] { $"{tableName}_Id" });
+            //    RenameColumn($"dbo.{child}s", $"{tableName}_Id", $"old_{tableName}_Id");
+            //    if (toGuid)
+            //    {
+            //        AddColumn($"dbo.{child}s", $"{tableName}_Id", c => c.Guid());
+            //    }
+            //    else
+            //    {
+            //        AddColumn($"dbo.{child}s", $"{tableName}_Id", c => c.Int());
+            //    }
+            //    Sql($"update c set {tableName}_Id=p.Id2 from {child}s c inner join {tableName}s p on p.Id=c.old_{tableName}_Id");
+            //    DropColumn($"dbo.{child}s", $"old_{tableName}_Id");
+            //}
+            
+            //foreach (var child in children)
+            //{
+            //    CreateIndex($"dbo.{child}", $"{tableName}_Id");
+            //    AddForeignKey($"dbo.{child}", $"{tableName}_Id", $"dbo.{tableName}", "Id");
+            //}
+        }
     }
+
 }
